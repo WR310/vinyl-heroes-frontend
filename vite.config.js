afceb15e31d0832daf32df5ae8e7ffeffede2a3c
WR310 <ismailwrwrwr@gmail.com>
2026-04-05 08:22:49 +0300

    add PWA assets and config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Vinyl Heroes',
+        short_name: 'Vinyl',
+        description: 'Vinyl Heroes Game',
+        theme_color: '#242424',
+        background_color: '#242424',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
